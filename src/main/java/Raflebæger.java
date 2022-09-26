3a1e7b2bd5a13efd1ab7e5311648520da5cfdd6b
Tine-m <tinemarbjerg@gmail.com>
2022-09-26 10:06:52 +0200

    fælles arbejde i klassen

--- src/main/java/Raflebæger.java
+++ src/main/java/Raflebæger.java
@@ -1,12 +1,27 @@
+import java.util.Random;
+
 public class Raflebæger {
     private int antalTerninger;
+    //array af Terning objekter???
 
     public Raflebæger(int antalTerninger) {
         this.antalTerninger = antalTerninger;
     }
 
     public int ryst(){
-        //simulere ryst med to "terninger"
-        return 10;
+        Random random = new Random();//random generator
+
+        int sum = 0;
+        //loope svarende til antalterninger
+        for(int i = 0; i< antalTerninger; i++ ) {
+            //lave tilfældigt tal ml 1-6
+            int randomNumber = random.nextInt(6) + 1;
+
+            // summere og returnere samlet  værdi
+            sum = sum + randomNumber;
+        }
+
+        return sum;
     }
 }
+
